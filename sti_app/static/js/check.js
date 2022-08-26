4534885af3226e8ba56018ca9a7db129e4a75d46
Himani <global.himani26@gmail.com>
2022-08-26 15:34:28 +0530

    charts in indicators added

--- sti_app/static/js/check.js
+++ sti_app/static/js/check.js
@@ -71,10 +71,109 @@ $(document).ready(function(){
                                     var indicator_label = value["indicator_title"];
                                     var indicator_def = value["indicator_description"];
                                     var indicator_id = value["id"];
+
                                     var template = '<div class="col-lg-12 col-md-12 text-center"><strong>'+indicator_label+'</strong></div><div class="col-lg-12 col-md-12">'+indicator_def+'</div>';
 
                                     indicator2.style.display = "flex";
                                     $("#indicator2").append(template);
+                                    var myDiv = document.getElementById("chart-container");
+                                    var divBarChart = document.getElementById("barchart-container");
+                                    if(myDiv){
+                                        const dataSource = {
+                                        chart: {
+                                        caption: "New Companies Registered <br> Year 2020-21",
+                                        subCaptionFontSize:"20",
+                                        plottooltext: "<b>$dataValue</b> of Firms/Companies Registered",
+                                        showlegend: "1",
+                                        showpercentvalues: "1",
+                                        legendposition: "bottom",
+                                        usedataplotcolorforlabels: "1",
+                                        labelFontSize: "20",
+                                        showPercentValues: "0",
+                                        theme: "fusion"
+                                        },
+                                        data: [
+                                        {
+                                        label: "New Indian Firms Registered",
+                                        toolText: "$dataValue",
+                                        color: "#483a75",
+                                        value: "2070"
+                                        },
+                                        {
+                                        label: "New LLP's Registered",
+                                        toolText: "$dataValue",
+                                        color: "#1cadad",
+                                        value: "519"
+                                        },
+                                        {
+                                        label: "New Foreign Firms Registered ",
+                                        toolText: "$dataValue",
+                                        color: "#de6172",
+                                        value: "0"
+                                        },
+                                        {
+                                        label: "New Companies Registered",
+                                        toolText: "$dataValue",
+                                        Color: "#ff9117",
+                                        value: "2589"
+                                        }
+                                        ]
+                                        };
+
+                                        FusionCharts.ready(function() {
+                                        var myChart = new FusionCharts({
+                                        type: "pie2d",
+                                        renderAt: "chart-container",
+                                        width: "100%",
+                                        height: "100%",
+                                        dataFormat: "json",
+                                        dataSource
+                                        }).render();
+                                        });
+                                    }
+                                    else if(divBarChart)
+                                    {
+                                        const dataSource = {
+                                          chart: {
+                                            caption: "Ease of Doing Business",
+                                            xaxisname: "Year",
+                                            yaxisname: "Ranking of State",
+
+                                            theme: "fusion"
+                                          },
+                                          data: [
+                                            {
+                                              label: "2017-18",
+                                              value: "20",
+                                              color: "#1cadad",
+                                            },
+                                            {
+                                              label: "2018-19",
+                                              value: "NA"
+                                            },
+                                            {
+                                              label: "2019-20",
+                                              value: "19",
+                                              color: "#ff9117",
+                                            },
+                                            {
+                                              label: "2020-21",
+                                              value: "NA"
+                                            },
+                                          ]
+                                        };
+
+                                        FusionCharts.ready(function() {
+                                          var myChart = new FusionCharts({
+                                            type: "column2d",
+                                            renderAt: "barchart-container",
+                                            width: "100%",
+                                            height: "100%",
+                                            dataFormat: "json",
+                                            dataSource
+                                          }).render();
+                                        });
+                                    }
                                 });
                             }
 
